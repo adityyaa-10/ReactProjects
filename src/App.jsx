daceab97991493c00e3959f8324944d593505524
Aditya <aditya.pandey.241103@gmail.com>
2022-11-11 21:41:04 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import styles from "./style";
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA,  Footer,} 
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA,  Footer} 
 from './components';
 
 const App = () =>
